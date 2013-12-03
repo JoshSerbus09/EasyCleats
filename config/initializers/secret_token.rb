7e7c0df70e7f7b1213cbb3d12c0fe83e71d47411
Josh Serbus <JoshSerbus09@gmail.com>
2013-12-02 20:16:51 -0600

    pictures posted, need to add jquery for animation, site layout complete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyCleats::Application.config.secret_token = '5c5d7dc7df1bd3af090f04a7a7428a6298c53de4a4d3f09056d698a7ae0d5709e599c8279ab56ad3cd7103b75976c0feb6dc74878884d1a3894592bcf0bf47b1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EasyCleats::Application.config.session_store :cookie_store, key: '_EasyCleats_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EasyCleats::Application.config.session_store :active_record_store
